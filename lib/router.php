0642a88bc4f9811f4bf2ff4688ac9ad20cd65417
remko <remko@djneo.nl>
2014-12-11 21:10:30 +0100

    Fixed indenting

--- lib/router.php
+++ lib/router.php
@@ -10,45 +10,43 @@
  *
  * @param $className
  */
-function __autoload($className){
-    $loaded = false;
-    if (strpos($className,'Lib') != false){
-        if (file_exists(ROOTPATH.'/lib/'.$className . '.php')) {
-            include ROOTPATH.'/lib/' . $className . '.php';
-            $loaded = true;
-        }
-    }
-    if (strpos($className,'Controller') != false){
-        if (file_exists(ROOTPATH.'/Controller/'.$className . '.php')) {
-            include ROOTPATH.'/Controller/'.$className.'.php';
-            $loaded = true;
-        }
-    }
-    if (strpos($className,'View') !== false){
-        if (file_exists(ROOTPATH.'/View/'.$className . '.php')) {
-            include ROOTPATH.'/View/' . $className . '.php';
-            $loaded = true;
-        }
+	$loaded = false;
+	if (strpos($className, 'Lib') != false) {
+		if (file_exists(ROOTPATH . '/lib/' . $className . '.php')) {
+			include ROOTPATH . '/lib/' . $className . '.php';
+			$loaded = true;
+		}
+	}
+	if (strpos($className, 'Controller') != false) {
+		if (file_exists(ROOTPATH . '/Controller/' . $className . '.php')) {
+			include ROOTPATH . '/Controller/' . $className . '.php';
+			$loaded = true;
+		}
+	}
+	if (strpos($className, 'View') !== false) {
+		if (file_exists(ROOTPATH . '/View/' . $className . '.php')) {
+			include ROOTPATH . '/View/' . $className . '.php';
+			$loaded = true;
+		}
 
-    }
-    if (!$loaded){
-        //File not loaded, Write to error log
-        file_put_contents(ROOTPATH.'/error.txt',"[".date("H:i:s Y-m-d") ."]  Failed to load ".$className. "\r\n", FILE_APPEND);
-    }
-    if ($loaded){
-        //File not loaded, Write to error log
-        file_put_contents(ROOTPATH.'/debug.txt',"[".date("H:i:s Y-m-d") ."]  Successfully loaded ".$className. "\r\n", FILE_APPEND);
-    }
+	}
+	if (!$loaded) {
+		//File not loaded, Write to error log
+		file_put_contents(ROOTPATH . '/error.txt', "[" . date("H:i:s Y-m-d") . "]  Failed to load " . $className . "\r\n", FILE_APPEND);
+	}
+	if ($loaded) {
+		//File not loaded, Write to error log
+		file_put_contents(ROOTPATH . '/debug.txt', "[" . date("H:i:s Y-m-d") . "]  Successfully loaded " . $className . "\r\n", FILE_APPEND);
+	}
 
 }
 
-function router(){
+function router() {
 	//Starting Controller
 	$startingCon = 'homeController';
 	//Starting action
 	$startingAction = 'index';
 
 
-	return array ('Controller' => $startingCon,'Action' => $startingAction );
-
+	return array('Controller' => $startingCon, 'Action' => $startingAction);
 }
