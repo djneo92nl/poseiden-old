0a435b103c40a29ae1423e2c15a0f75d5c3d8c58
djneo <remko@djneo.nl>
2015-01-18 20:14:48 +0100

    [TASK] Router change for models

--- lib/router.php
+++ lib/router.php
@@ -5,6 +5,7 @@
  * @param $className
  */
 function __autoload ($className) {
+	//TODO: Rewrite this piece of s**t
 	$loaded = false;
 	if (strpos($className, 'Lib') != false) {
 		if (file_exists(ROOTPATH . '/lib/' . $className . '.php')) {
@@ -21,6 +22,13 @@ function __autoload ($className) {
 	if (strpos($className, 'View') !== false) {
 		if (file_exists(ROOTPATH . '/View/' . $className . '.php')) {
 			include ROOTPATH . '/View/' . $className . '.php';
+			$loaded = true;
+		}
+
+	}
+	if (strpos($className, 'Model') !== false) {
+		if (file_exists(ROOTPATH . '/Model' . $className . '.php')) {
+			include ROOTPATH . '/Model' . $className . '.php';
 			$loaded = true;
 		}
 
