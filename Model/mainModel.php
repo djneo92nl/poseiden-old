03e249cd8b49e36641c8e37960358320b3db5dad
djneo92nl <remko@djneo.nl>
2015-05-05 18:05:17 +0200

    [TASK] This should be in the PDO Lib

--- Model/mainModel.php
+++ Model/mainModel.php
@@ -1,12 +1,12 @@
 <?php
 
+namespace Poseiden;
+
 class mainModel {
 	/**
 	 * @var string database Name
 	 */
 	private $database;
-
-	private $dboHandler;
 
 	/**
 	 * @return mixed
@@ -22,27 +22,9 @@ class mainModel {
 		$this->database = $database;
 	}
 
-	public function __construct() {
-		//Include settings
-		include_once(ROOTPATH . '/lib/config.local.php');
-		$this->settings = $settings;
+
+	public function insert () {
+
 	}
-
-	public function setConnection () {
-
-		if (!isset($this->settings['database'])) {
-			debugMessage('Model ', null, 'No Config setting', false);
-
-		}
-		$this->dboHandler = new PDO('mysql:host=localhost;dbname=testdb;charset=utf8', $this->settings['database']['username'], 'password');
-		$this->dboHandler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$this->dboHandler->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-	}
-
-	public function getData() {
-		$stmt = $this->dboHandler->query("SELECT * FROM" . $this->database);
-		return $stmt->fetchAll(PDO::FETCH_ASSOC);
-	}
-
-
+	
 }
--- lib/pdolib.php
+++ lib/pdolib.php
@@ -7,4 +7,37 @@
  * Time: 21:30
  */
 class pdoLib extends main {
+
+    private $dboHandler;
+
+    public function __construct() {
+        //Include settings
+        include_once(ROOTPATH . '/lib/config.local.php');
+        $this->settings = $settings;
+    }
+
+    public function setConnection () {
+        if (!isset($this->settings['database'])) {
+            debugMessage('Model ', null, 'No Config setting', false);
+
+        }
+        try{
+            $this->dboHandler = new PDO('mysql:host=localhost;dbname=testdb;charset=utf8',
+                $this->settings['database']['username'],
+                $this->settings['database']['password']);
+            $this->dboHandler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->dboHandler->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        } catch (PDOException $e) {
+            debugMessage('ModelPDO', null, $e->getMessage(), false);
+        }
+    }
+
+    public function getData() {
+        $stmt = $this->dboHandler->query("SELECT * FROM" . $this->database);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+
+
 }
