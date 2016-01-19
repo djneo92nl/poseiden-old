e759b15c0da9424551665bb399473cc0aa4d0b94
Remco Janse <r.janse@youwe.nl>
2016-01-19 11:10:52 +0100

    [TASK] config location changed

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 namespace Poseiden\Core;
-
+use Poseiden\Core\Lib;
 
 //Define Main Variables
 define('ROOTPATH', __DIR__);
+define('CONFIGFOLDER', ROOTPATH . '/config/');
 define('DEVELOPMENT', TRUE);
 
 require_once (ROOTPATH . '/vendor/autoload.php');
 
-$app = new main();
+$app = new Lib\main();
